807eac7b57def68f1d5d873f29a218a33aca02d6
onabz <ONABOLUMBE@GMAIL.COM>
2021-12-04 18:13:39 +0000

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 const beginButton = document.getElementById('start-btn');
 const questionBox = document.getElementById('question-container');
+const mixQuestions, currentQuestionNumber
 
 beginButton.addEventListener('click', beginGame);
 
 function beginGame() {
     console.log('started');
     beginButton.classList.add('hide');
+    mixQuestions = questions.sort(() => Math.random() - .5);
     questionBox.classList.remove('hide');
+    nextQuestion()
 }
 
 function nextQuestion() {
@@ -16,3 +19,15 @@ function nextQuestion() {
 function chooseAnswer() {
 
 }
+
+const questions = [
+    {
+        question: 'Which Italian city is home of the car manufacturer Fiat?',
+        answers: [
+            {option: 'Turin', correct: true },
+            {option: 'Maranello', correct: false },
+            {option: 'Modena', correct: false },
+            {option: 'Rome', correct: false },                     
+        ]
+    }
+]
