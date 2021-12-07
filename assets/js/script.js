1b34da11fb9bf6be7f074a53485cbef385aaef85
onabz <ONABOLUMBE@GMAIL.COM>
2021-12-07 10:17:24 +0000

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,77 +5,6 @@ const answerItems = document.getElementById('answer-buttons');
 const nextButton = document.getElementById('next-btn');
 let mixQuestions, currentQuestionNumber;
 
-beginButton.addEventListener('click', beginGame);
-nextButton.addEventListener('click', () => {
-    currentQuestionNumber++  
-    nextQuestion()
-})
-
-function beginGame() {
-    beginButton.classList.add('vanish')
-    mixQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionNumber = 0
-    questionBox.classList.remove('vanish')
-    nextQuestion()
-}
-
-function nextQuestion() {
-    resetState()
-    getQuestion(mixQuestions[currentQuestionNumber])
-}
-
-function getQuestion(question) {
-    questionItem.innerText = question.question
-    question.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.option
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', chooseAnswer)
-        answerItems.appendChild(button)
-    })
-}
-
-function resetState() {
-    bodyStatusRemove(document.body)
-    nextButton.classList.add('vanish')
-    while (answerItems.firstChild) {
-      answerItems.removeChild(answerItems.firstChild)
-  }    
-}
-
-function chooseAnswer(e) {
-    const choosenButton = e.target
-    const right = choosenButton.dataset.correct
-    bodyStatus(document.body, right)
-    Array.from(answerItems.children).forEach(button => {
-        bodyStatus(button, button.dataset.correct)
-    })
-    if (mixQuestions.length > currentQuestionNumber + 1) {
-        nextButton.classList.remove('vanish')
-    }   else {
-        beginButton.innerText = 'Restart'
-        beginButton.classList.remove('vanish')
-    }
-    
-}
-
-function bodyStatus(entity, right) {
-    bodyStatusRemove(entity)
-    if (right) {
-        entity.classList.add('correct')
-    } else {
-        entity.classList.add('wrong')
-    }
-}
-
-function bodyStatusRemove(entity) {
-    entity.classList.remove('correct')
-    entity.classList.remove('wrong')
-}
-
 const questions = [
     {
         question: 'Which Italian city is home of the car manufacturer Fiat?',
@@ -338,3 +267,75 @@ const questions = [
         ]
     }
 ]
+
+beginButton.addEventListener('click', beginGame);
+nextButton.addEventListener('click', () => {
+    currentQuestionNumber++  
+    nextQuestion()
+})
+
+function beginGame() {
+    beginButton.classList.add('vanish')
+    mixQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionNumber = 0
+    questionBox.classList.remove('vanish')
+    nextQuestion()
+}
+
+function nextQuestion() {
+    resetState()
+    getQuestion(mixQuestions[currentQuestionNumber])
+}
+
+function getQuestion(question) {
+    questionItem.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.option
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', chooseAnswer)
+        answerItems.appendChild(button)
+    })
+}
+
+function resetState() {
+    bodyStatusRemove(document.body)
+    nextButton.classList.add('vanish')
+    while (answerItems.firstChild) {
+      answerItems.removeChild(answerItems.firstChild)
+  }    
+}
+
+function chooseAnswer(e) {
+    const choosenButton = e.target
+    const right = choosenButton.dataset.correct
+    bodyStatus(document.body, right)
+    Array.from(answerItems.children).forEach(button => {
+        bodyStatus(button, button.dataset.correct)
+    })
+    if (mixQuestions.length > currentQuestionNumber + 1) {
+        nextButton.classList.remove('vanish')
+    }   else {
+        beginButton.innerText = 'Restart'
+        beginButton.classList.remove('vanish')
+    }
+    
+}
+
+function bodyStatus(entity, right) {
+    bodyStatusRemove(entity)
+    if (right) {
+        entity.classList.add('correct')
+    } else {
+        entity.classList.add('wrong')
+    }
+}
+
+function bodyStatusRemove(entity) {
+    entity.classList.remove('correct')
+    entity.classList.remove('wrong')
+}
+
